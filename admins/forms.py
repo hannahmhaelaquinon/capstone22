78af17762ec92787be6599d4e18a66c342f02163
mhirang <trisha18.cruz@gmail.com>
2022-12-15 13:21:40 +0800

    level forms - admin side

--- admins/forms.py
+++ admins/forms.py
@@ -8,9 +8,10 @@ class SectionForm(forms.ModelForm):
         model = models.Section
         fields = '__all__'
 
-
-
-
+class LevelForm(forms.ModelForm):
+    class Meta:
+        model = models.Levels
+        fields = '__all__'
 
 class ContactusForm(forms.Form):
     Name = forms.CharField(max_length=30)
